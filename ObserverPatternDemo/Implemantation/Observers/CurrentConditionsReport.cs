3bdce10c0673bfb8ef13ccd6e09573a29b387807
AnzhelikaKravchuk <https://github.com/AnzhelikaKravchuk/>
2018-11-04 21:06:29 +0300

    Rollback to original vertion

--- ObserverPatternDemo/Implemantation/Observers/CurrentConditionsReport.cs
+++ ObserverPatternDemo/Implemantation/Observers/CurrentConditionsReport.cs
@@ -1,31 +1,12 @@
-﻿using System;
-using System.Threading;
-using ObserverPatternDemo.Implemantation.Observable;
+﻿using ObserverPatternDemo.Implemantation.Observable;
 
 namespace ObserverPatternDemo.Implemantation.Observers
 {
-    /// <summary>
-    /// Represents current conditions report.
-    /// </summary>
     public class CurrentConditionsReport : IObserver<WeatherInfo>
     {
-        private WeatherInfo currentWeatherInformation;
-
-        private static readonly Lazy<CurrentConditionsReport> instance =
-            new Lazy<CurrentConditionsReport>(() => new CurrentConditionsReport(), LazyThreadSafetyMode.PublicationOnly);
-
-        public static CurrentConditionsReport Instance { get => instance.Value; }
-
-        private CurrentConditionsReport() { }
-
         public void Update(IObservable<WeatherInfo> sender, WeatherInfo info)
         {
-            currentWeatherInformation = info;
-        }
-
-        public string GetCurrentConditionsReport()
-        {
-            return currentWeatherInformation.ToString();
+            throw new System.NotImplementedException();
         }
     }
 }
--- ObserverPatternDemo/Implemantation/Observers/StatisticReport.cs
+++ ObserverPatternDemo/Implemantation/Observers/StatisticReport.cs
@@ -1,59 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using ObserverPatternDemo.Implemantation.Observable;
 
 namespace ObserverPatternDemo.Implemantation.Observers
 {
-    /// <summary>
-    /// Contains methods that help to constitute the weather conditions statistic.
-    /// </summary>
-    /// <seealso cref="ObserverPatternDemo.IObserver{ObserverPatternDemo.Implemantation.Observable.WeatherInfo}" />
     public class StatisticReport : IObserver<WeatherInfo>
     {
-        private static readonly Lazy<StatisticReport> instance =
-            new Lazy<StatisticReport>(() => new StatisticReport(), LazyThreadSafetyMode.PublicationOnly);
-
-        public static StatisticReport Instance { get => instance.Value; }
-
-        private StatisticReport() { }
-
-        private List<WeatherInfo> listOfWeatherConditions = new List<WeatherInfo>();
-
-        /// <summary>
-        /// Adds weather notifications in special repository.
-        /// </summary>
-        /// <param name="sender">The object that is to raised notifications.</param>
-        /// <param name="info">The current notification information.</param>
         public void Update(IObservable<WeatherInfo> sender, WeatherInfo info)
         {
-            listOfWeatherConditions.Add(info);
-        }
-
-        /// <summary>
-        /// Gets the statistic report.
-        /// </summary>
-        /// <returns>Statistic report in string representation.</returns>
-        public string GetStatisticReport()
-        {
-            if (listOfWeatherConditions.Count == 0)
-            {
-                return string.Empty;
-            }
-
-            StringBuilder statistic = new StringBuilder();
-            statistic.Append('*', 30);
-            statistic.Append(" Statistic Report ");
-            statistic.Append('*', 30);
-            statistic.Append("\n");
-
-            for (int i = 0; i < listOfWeatherConditions.Count; i++)
-            {
-                statistic.Append(listOfWeatherConditions[i] + "\n");
-            }
-            statistic.Append('*', 78);
-            return statistic.ToString();
+            throw new NotImplementedException();
         }
     }
 }
