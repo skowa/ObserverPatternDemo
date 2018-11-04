7b79acc8f3dda47154c1e59756aa9f9c22caa11d
Stanislav Koburneev <koburneev.stanislav@gmail.com>
2018-11-04 18:57:50 +0300

    Added functionality to the classes.

--- ObserverPatternDemo/Implemantation/Observers/CurrentConditionsReport.cs
+++ ObserverPatternDemo/Implemantation/Observers/CurrentConditionsReport.cs
@@ -1,13 +1,31 @@
-﻿using ObserverPatternDemo.Implemantation.Observable;
-using System;
+﻿using System;
+using System.Threading;
+using ObserverPatternDemo.Implemantation.Observable;
 
 namespace ObserverPatternDemo.Implemantation.Observers
 {
+    /// <summary>
+    /// Represents current conditions report.
+    /// </summary>
     public class CurrentConditionsReport : IObserver<WeatherInfo>
     {
+        private WeatherInfo currentWeatherInformation;
+
+        private static readonly Lazy<CurrentConditionsReport> instance =
+            new Lazy<CurrentConditionsReport>(() => new CurrentConditionsReport(), LazyThreadSafetyMode.PublicationOnly);
+
+        public static CurrentConditionsReport Instance { get => instance.Value; }
+
+        private CurrentConditionsReport() { }
+
         public void Update(IObservable<WeatherInfo> sender, WeatherInfo info)
         {
-            throw new NotImplementedException();
+            currentWeatherInformation = info;
+        }
+
+        public string GetCurrentConditionsReport()
+        {
+            return currentWeatherInformation.ToString();
         }
     }
 }
--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ObserverPatternDemo.Implemantation.Observable;
+using ObserverPatternDemo.Implemantation.Observers;
 
 namespace WeatherStation
 {
@@ -10,6 +8,34 @@ namespace WeatherStation
     {
         static void Main(string[] args)
         {
+            WeatherInfo weather_1 = new WeatherInfo(16, 20, 101322);
+            WeatherInfo weather_2 = new WeatherInfo(22, 26, 101324);
+            WeatherInfo weather_3 = new WeatherInfo(25, 22, 101326);
+            WeatherInfo weather_4 = new WeatherInfo(17, 40, 101328);
+            WeatherInfo weather_5 = new WeatherInfo(19, 23, 101330);
+
+            WeatherData weatherData = WeatherData.Instance;
+
+            CurrentConditionsReport report = CurrentConditionsReport.Instance;
+            StatisticReport statistic = StatisticReport.Instance;
+
+            weatherData.Register(report);
+            weatherData.Register(statistic);
+
+            weatherData.Notify(weatherData, weather_1);
+            weatherData.Notify(weatherData, weather_2);
+            weatherData.Notify(weatherData, weather_3);
+
+            Console.WriteLine(report.GetCurrentConditionsReport());
+            Console.WriteLine(statistic.GetStatisticReport());
+
+            weatherData.Unregister(statistic);
+
+            weatherData.Notify(weatherData, weather_4);
+            weatherData.Notify(weatherData, weather_5);
+
+            Console.WriteLine(report.GetCurrentConditionsReport());
+            Console.WriteLine(statistic.GetStatisticReport());
         }
     }
 }
